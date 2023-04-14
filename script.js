7be145843370949a01e9fc9ac8089ac6a3a050ce
Claudiu Zavate <37959676+Zellenfortex@users.noreply.github.com>
2023-04-15 01:26:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-const hamburger = document.querySelector("#menu");
-const nav = document.querySelector("ul");
+// Carousel img - header
+const buttons = document.querySelectorAll("[data-carousel-button]");
+const nextButton = document.querySelector("[data-carousel-button='next']");
+let intervalId;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    clearInterval(intervalId); // reset interval on click
+
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+
+    intervalId = setInterval(autoSwipe, 7000); // restart interval after click
+  });
+});
+
+// Auto Swipe
+function autoSwipe() {
+  nextButton.click();
+}
+intervalId = setInterval(autoSwipe, 5000); // start first interval 
+
+// Hamburger animation active
+
+const hamburger = document.querySelector(".hamburger");
+const layers = document.querySelectorAll(".hamburger div");
 
 hamburger.addEventListener("click", () => {
-    nav.classList.toggle('active');
-})
+  layers.forEach((layers) => layers.classList.toggle("activeHamburger"));
+});
+
+// Toggle Mobile Menu
+
+const nav = document.querySelector("nav ul");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  nav.classList.toggle("MobileMenu");
+});
